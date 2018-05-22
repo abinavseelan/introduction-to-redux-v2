90dde5e3387673306075e794881ada7703691303
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-05-23 00:49:00 +0530

    Demo 3 base

--- src/app.js
+++ src/app.js
@@ -1,82 +1,32 @@
 import './app.css';
 
-// Import the `combineReducers` method
-import { createStore, combineReducers } from 'redux';
+// import createStore and combineReducers
 
-const initialCartState = {
-    items: [],
-    amount: 0,
-}
+const amountContainer = document.getElementById('amount');
+const itemsContainer = document.getElementById('items');
+const productContainer = document.getElementById('product-container');
 
-const initialProductsState = {
-    items: [],
-};
+productContainer.addEventListener('click', (event) => {
+    const { action, item } = event.target.dataset;
 
-// Create two reducers
-function cartReducer(state = initialCartState, action) {
-    let newState;
-
-    if (action.type === 'ADD_ITEM_TO_CART') {
-        newState = {
-            ...state,
-            items: [...state.items, action.item],
-            amount: state.amount + action.item.price,
-        }
-    } else {
-        newState = state;
-    }
-
-    return newState;
-}
-
-function productsReducer(state = initialProductsState, action) {
-    let newState;
-
-    if (action.type === 'ADD_ITEM') {
-        newState = {
-            ...state,
-            items: [...state.items, action.item],
-        }
-    } else {
-        newState = state;
-    }
-
-    return newState;
-}
-
-// Combine the reducers
-const rootReducer = combineReducers({
-    cart: cartReducer,
-    product: productsReducer,
+    // dispatch add item action here
 });
 
-// Create the store using the combined reducer
-const store = createStore(rootReducer);
+function updateItems(itemsContainer, items) {
+    itemsContainer.innerHTML = '';
 
-// store.getState()
-console.log("========= Store ========");
-console.log(store.getState());
-
-// store.subscribe() to subscribe to store updates.
-store.subscribe(function () {
-    console.log("======== Store updated ========");
-    console.log(store.getState());
-});
-
-function addItemToCart(item) {
-    return {
-        type: 'ADD_ITEM_TO_CART',
-        item,
-    }
+    itemsContainer.innerHTML = items.map(item => `
+        <div>
+            ${item.name}: ${item.quantity}
+        </div>
+    `);
 }
 
-// Update the actions to an action creator
-store.dispatch(addItemToCart({
-    name: 'Oneplus 6',
-    price: 34000
-}));
 
-store.dispatch(addItemToCart({
-    name: 'Samsung s9',
-    price: 64000
-}));
+// create reducers
+
+// combine reducers
+
+// create store
+
+// subscribe to the store and call updateItems()
