653818af173089aab6defb705f35d9d3c5a98c54
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-05-22 23:09:05 +0530

    Demo 2 complete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,57 @@
 import './app.css';
 
 // Import the `combineReducers` method
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const initialState = {
+const initialCartState = {
     items: [],
     amount: 0,
+}
+
+const initialProductsState = {
+    items: [],
 };
 
 // Create two reducers
+function cartReducer(state = initialCartState, action) {
+    let newState;
+
+    if (action.type === 'ADD_ITEM_TO_CART') {
+        newState = {
+            ...state,
+            items: [...state.items, action.item],
+            amount: state.amount + action.item.price,
+        }
+    } else {
+        newState = state;
+    }
+
+    return newState;
+}
+
+function productsReducer(state = initialProductsState, action) {
+    let newState;
+
+    if (action.type === 'ADD_ITEM') {
+        newState = {
+            ...state,
+            items: [...state.items, action.item],
+        }
+    } else {
+        newState = state;
+    }
+
+    return newState;
+}
 
 // Combine the reducers
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    product: productsReducer,
+});
 
 // Create the store using the combined reducer
-const store = createStore();
+const store = createStore(rootReducer);
 
 // store.getState()
 console.log("========= Store ========");
@@ -25,19 +63,20 @@ store.subscribe(function () {
     console.log(store.getState());
 });
 
-// Update the actions to an action creator
-store.dispatch({
-    type: 'ADD_ITEM_TO_CART',
-    item: {
-        name: 'OnePlus 6',
-        price: 34000,
+function addItemToCart(item) {
+    return {
+        type: 'ADD_ITEM_TO_CART',
+        item,
     }
-});
+}
 
-store.dispatch({
-    type: 'ADD_ITEM_TO_CART',
-    item: {
-        name: 'Samsung s9',
-        price: 64000,
-    }
-});
+// Update the actions to an action creator
+store.dispatch(addItemToCart({
+    name: 'Oneplus 6',
+    price: 34000
+}));
+
+store.dispatch(addItemToCart({
+    name: 'Samsung s9',
+    price: 64000
+}));
