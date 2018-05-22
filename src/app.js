34e4dea9907126517c75dfcefe5cf7b25a74c3c8
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-05-22 22:42:15 +0530

    Base for demo #1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import './app.css';
+
+// Import the `createStore` method
+
+// Create a reducer
+
+// Create the store using the reducer
+
+// store.getState()
+
+// store.dispatch() can be used to dispatch an action
+
+// Maybe dispatch another action? :)
+
+// store.subscribe() to subscribe to store updates.
