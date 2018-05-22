7bb024916c80210b75d86259cac44bf66da1f57d
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-05-23 01:18:46 +0530

    demo 3 complete

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import './app.css';
+import { combineReducers, createStore } from 'redux';
 
 // import createStore and combineReducers
 
@@ -18,6 +19,39 @@ function updateItems(itemsContainer, items) {
 }
 
 // create action creators
+function updateAmount(amountContainer, amount) {
+    amountContainer.innerHTML = `Rs. ${amount}`;
+}
+
+function updateSelected(selectedContainer, selected) {
+    selectedContainer.innerHTML = selected;
+}
+
+function addItem(item) {
+    return {
+        type: 'ADD_ITEM',
+        item,
+    }
+};
+
+function removeItem(item) {
+    return {
+        type: 'REMOVE_ITEM',
+        item,
+    }
+};
+
+function inc() {
+    return {
+        type: 'INC_COUNT',
+    }
+};
+
+function dec() {
+    return {
+        type: 'DEC_COUNT',
+    }
+};
 
 // create reducers
 const initialCartState = {
@@ -32,16 +66,80 @@ const initialProductState = {
     selected: 0,
 };
 
+function cartReducer(state = initialCartState, action) {
+    let newState;
+
+    if (action.type === 'ADD_ITEM') {
+        newState = {
+            ...state,
+            items: {
+                ...state.items,
+                [action.item]: state.items[action.item] + 1,
+            },
+            amount: action.item === 'pizza' ? state.amount + 300 : state.amount + 150,
+        };
+    } else if (action.type === 'REMOVE_ITEM') {
+        newState = {
+            ...state,
+            items: {
+                ...state.items,
+                [action.item]: state.items[action.item] - 1,
+            },
+            amount: action.item === 'pizza' ? state.amount - 300 : state.amount - 150,
+        };
+    } else {
+        newState = state;
+    }
+
+    return newState;
+}
+
+function productReducer(state = initialProductState, action) {
+    let newState;
+
+    if (action.type === 'INC_COUNT') {
+        newState = {
+            ...state,
+            selected: state.selected + 1,
+        }
+    } else if (action.type === 'DEC_COUNT') {
+        newState = {
+            ...state,
+            selected: state.selected - 1,
+        }
+    } else {
+        newState = state;
+    }
+
+    return newState;
+}
+
 // combine reducers
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    products: productReducer,
+})
 
 // create store
+const store = createStore(rootReducer);
 
 // subscribe to the store and call updateItems()
-
+store.subscribe(function () {
+    updateItems(itemsContainer, store.getState().cart.items);
+    updateAmount(amountContainer, store.getState().cart.amount);
+    updateSelected(selectedContainer, store.getState().products.selected);
+});
 
 productContainer.addEventListener('click', (event) => {
     const { action, item } = event.target.dataset;
 
     // dispatch add item action here
     // dispatch inc count action here
+    if (action === 'add') {
+        store.dispatch(addItem(item));
+        store.dispatch(inc());
+    } else if (action === 'remove') {
+        store.dispatch(removeItem(item));
+        store.dispatch(dec());
+    }
 });
